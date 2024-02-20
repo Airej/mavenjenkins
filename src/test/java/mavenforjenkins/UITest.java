39e7dd6acab412b9fbdca9e6ae369bb73e514750
Airej <101320060+Airej@users.noreply.github.com>
2024-02-20 12:40:58 +0500

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -5,36 +5,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class UITest 
-{
+public class UITest {
 
-	@Parameters("Browser")
-	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
-		}
-		else if(browserName.contains("Edge"))
-		{
-			 driver=new EdgeDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
-	}
-	
-	
+    private WebDriver driver; // Declaring driver at the class level
+
+    @Parameters("Browser")
+    @BeforeMethod
+    public void setUp(String browserName) {
+        System.out.println("Parameter value is " + browserName);
+
+        if (browserName.contains("Chrome")) {
+            ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("--headless");
+            opt.addArguments("--no-sandbox");
+            opt.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(opt);
+        } else if (browserName.contains("Edge")) {
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void verifyTitle() {
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
