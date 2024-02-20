29c33025b0149aa45047cadac3bb683cfe6ea680
Airej <101320060+Airej@users.noreply.github.com>
2024-02-20 12:55:21 +0500

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -8,33 +8,38 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class UITest 
-{
+public class UITest {
 
-	@Parameters("Browser")
-	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
-		}
-		else if(browserName.contains("Edge"))
-		{
-			 driver=new EdgeDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
-	}
-	
-	
+    @Parameters("Browser")
+    @Test
+    public void startBrowser(String browserName) {
+        System.out.println("Parameter value is " + browserName);
+        WebDriver driver = initializeDriver(browserName);
+
+        if (driver != null) {
+            driver.get("https://opensource-demo.orangehrmlive.com/");
+            Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+            driver.quit();
+        } else {
+            Assert.fail("Driver initialization failed. Check browser parameter.");
+        }
+    }
+
+    private WebDriver initializeDriver(String browserName) {
+        WebDriver driver = null;
+
+        if (browserName.contains("Chrome")) {
+            ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("--headless");
+            opt.addArguments("--no-sandbox");
+            opt.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(opt);
+            driver.manage().window().maximize(); // Maximize the window after initialization
+        } else if (browserName.contains("Edge")) {
+            driver = new EdgeDriver();
+            driver.manage().window().maximize(); // Maximize the window after initialization
+        }
+
+        return driver;
+    }
 }
